89ce4ae49997be702611fadfd4fbeb0de9877762
Christopher Geary <dev@crgeary.com>
2016-02-04 08:43:54 +0000

    Fix asset dependency handles

--- functions.php
+++ functions.php
@@ -3,20 +3,20 @@
 if (!function_exists('swift_enqueue_assets')) {
     /**
      * Enqueue JavaScript & CSS assets. The WordPress way.
-     * 
+     *
      * @return void
      */
     function swift_enqueue_assets()
     {
         $theme = untrailingslashit(get_template_directory_uri());
-        
-        wp_enqueue_script('angularjs', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js');
-        wp_enqueue_script('angularjs-router', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js');
 
-        wp_enqueue_script('swift-app', $theme.'/assets/js/app.js', ['angularjs', 'angular-route']);
-        
+        wp_enqueue_script('angularjs', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js');
+        wp_enqueue_script('angularjs-route', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js');
+
+        wp_enqueue_script('swift-app', $theme.'/assets/js/app.js', ['angularjs', 'angularjs-route']);
+
         wp_localize_script('swift-app', 'swift', [
-            'root' => esc_url(home_url()), 
+            'root' => esc_url(home_url()),
             'templates' => $theme.'/templates/'
         ]);
 
