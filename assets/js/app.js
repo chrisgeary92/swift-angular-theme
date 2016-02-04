9642d99271e4dd00f66e6aff36308ad9ff80190b
Christopher Geary <dev@crgeary.com>
2016-02-04 14:22:20 +0000

    Added search form & single posts view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'ngSanitize']);
 
 app.config(function($routeProvider, $locationProvider) {
-    
-    $routeProvider.when('/', {
-        templateUrl: swift.templates + '/home.html',
-        controller: 'Home'
-    });
+
+    $routeProvider
+        .when('/', {
+            templateUrl: swift.templates + '/home.html',
+            controller: 'Home'
+        })
+        .when('/:slug/', {
+            templateUrl: swift.templates + '/single.html',
+            controller: 'Single'
+        });
 
     $locationProvider.html5Mode(true);
 
@@ -14,5 +19,36 @@ app.config(function($routeProvider, $locationProvider) {
 app.controller('Home', function($scope, $http, $routeParams) {
     $http.get(swift.root + '/wp-json/wp/v2/posts').success(function(res) {
         $scope.posts = res;
+        document.querySelector('title').innerHTML = 'Home | ' + swift.site_name;
     });
+});
+
+app.controller('Single', function($scope, $http, $routeParams) {
+    $http.get(swift.root + '/wp-json/wp/v2/posts?filter[name]=' + $routeParams.slug).success(function(res) {
+        $scope.post = res[0];
+        document.querySelector('title').innerHTML = res[0].title.rendered + ' | ' + swift.site_name;
+    });
+});
+
+app.filter('toTrusted', ['$sce', function($sce) {
+    return function(text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);
+
+app.directive('swiftSearchForm', function() {
+    return {
+        restrict: 'E', // E = element, A = attribute, C = classname
+        templateUrl: swift.templates + '/search-form.html',
+        controller: function($scope, $http) {
+            $scope.filter = {
+                s: ''
+            };
+            $scope.search = function() {
+                $http.get(swift.root + '/wp-json/wp/v2/posts?filter[s]=' + $scope.filter.s).success(function(res) {
+                    $scope.posts = res;
+                });
+            };
+        }
+    };
 });
--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
 
+if (!function_exists('swift_theme_support')) {
+    /**
+     * Register theme support for HTML5, <title> & feed links
+     *
+     * @return void
+     */
+    function swift_theme_support()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5', ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption']);
+    }
+    add_action('after_setup_theme', 'swift_theme_support');
+}
+
 if (!function_exists('swift_enqueue_assets')) {
     /**
      * Enqueue JavaScript & CSS assets. The WordPress way.
@@ -12,12 +27,14 @@ if (!function_exists('swift_enqueue_assets')) {
 
         wp_enqueue_script('angularjs', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js');
         wp_enqueue_script('angularjs-route', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js');
+        wp_enqueue_script('angularjs-sanitize', '//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-sanitize.min.js');
 
-        wp_enqueue_script('swift-app', $theme.'/assets/js/app.js', ['angularjs', 'angularjs-route']);
+        wp_enqueue_script('swift-app', $theme.'/assets/js/app.js', ['angularjs', 'angularjs-route', 'angularjs-sanitize']);
 
         wp_localize_script('swift-app', 'swift', [
             'root' => esc_url(home_url()),
-            'templates' => $theme.'/templates/'
+            'templates' => $theme.'/templates',
+            'site_name' => get_bloginfo('name')
         ]);
 
         wp_enqueue_style('swift-css', get_stylesheet_uri());
